425f35c47a52e3b709803efdb8418f3e285c2433
Michael McKenzie <mikemckenzie987@gmail.com>
2018-08-03 16:19:18 -0400

    using prev functions

--- kata.js
+++ kata.js
@@ -1,52 +1,52 @@
+let addme = add(2, 4);
+
 console.log("Add: 1 Point");
 function add(x, y) {
-    return (x + y)
+    return x + y
 }
 console.log(add(2, 4));
 
 
+let multme = multiply(addme, 8)
+
 console.log("Multiply: 2 Points");
-function multiply(a, b) {
-    var c = 0
+function multiply(addme, b) {
+    let result = 0
     for (let counter = 1; counter <= b; counter++) {
-        c = c + a
+        result = add(addme, result)
     }
-    return (c)
+    return (result)
 }
 console.log(multiply(6, 8));
 
 
 console.log("Power: 2 Points");
 function Power(a, b) {
-    var c = 1
+    let result = 1
     for (let counter = 1; counter <= b; counter++) {
-        c = c * a
+        result = multiply(a, result)
     }
-    return (c)
+    return (result)
 }
 console.log(Power(2, 8));
 
 
 console.log("Factorial: 2 Points");
 function factorialize(counter) {
-    let result = counter;
-    if (counter === 0 || counter === 1) {
-        return 1;
+    let result = 1
+    for (let counter = 4; counter>0; counter--) {
+        result = multiply(counter, result)
     }
-    while (counter > 1) {
-        counter--;
-        result = multiply(counter, result);
-    }
-    return result;
+    return (result)
 }
 console.log(factorialize(4));
 
 
 console.log("Fibonacci: 3 Points");
     function fibonacci(n) {
-    var numberbeforeresult = 1;
-    var twobeforeresult = 1;
-    var result = 0
+    let numberbeforeresult = 1;
+    let twobeforeresult = 1;
+    let result = 0
     for (let counter = 1; counter < n; counter++) {
         twobeforeresult = numberbeforeresult
         numberbeforeresult = result
